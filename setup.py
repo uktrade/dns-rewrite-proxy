5b1a5ee713a3aba997a1bf6aaffd001cb815886a
Michal Charemza <michal@charemza.name>
2020-01-21 18:03:49 +0000

    (release) v0.0.13

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def long_description():
 
 setuptools.setup(
     name='dnsrewriteproxy',
-    version='0.0.12',
+    version='0.0.13',
     author='Department for International Trade',
     author_email='webops@digital.trade.gov.uk',
     description='A DNS proxy server that conditionally rewrites and filters A record requests',
